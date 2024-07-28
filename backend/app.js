18c59d5ee42953be0f12e9f780084da4a34b7681
Dev-Yadnesh8 <yadnesh.narawade22@gmail.com>
2024-07-28 10:57:28 +0530

    init commit

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
 
+// setting up middlewares 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+
+// logic and routes for app 
+app.get('/',(req,res)=>{
+    res.send('Hey');
+});
+
+app.listen(3000);
---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/mini-project-1');
+
+const userSchema =   mongoose.Schema({
+  userName : String,
+  age : Number,
+  email: String,
+  password : String,
+});
+
+module.exports = mongoose.model('user',userSchema);
